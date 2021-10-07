9d0636177afd332d7707ad2bb8e1368b1bf832eb
tramalho <thiagofacul@gmail.com>
2021-10-06 22:09:57 -0300

    init

---
+++ iamrich-swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iamrich-swiftui
+//
+//  Created by Thiago Antonio Ramalho on 06/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iamrich-swiftui/iamrich_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  iamrich_swiftuiApp.swift
+//  iamrich-swiftui
+//
+//  Created by Thiago Antonio Ramalho on 06/10/21.
+//
+
+import SwiftUI
+
+@main
+struct iamrich_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
